cdef1eddf0a97e53c5ed620033324051acb6cdd7
Guilherme <guilhermelacerdaa3@gmail.com>
2025-04-29 22:03:39 -0300

    Alterações finais.

--- backend/Scripts/script_criacao.sql
+++ backend/Scripts/script_criacao.sql
@@ -1,10 +1,9 @@
-CREATE TABLE `armarios` (
+ALTER TABLE doacoes add column locais varchar(200) NOT NULL; CREATE TABLE `armarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `localizacao` varchar(255) NOT NULL,
   `senha_acesso` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 
 CREATE TABLE `doacoes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -12,11 +11,11 @@ CREATE TABLE `doacoes` (
   `tipo` varchar(100) NOT NULL,
   `quantidade` int(11) NOT NULL,
   `data_doacao` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `instituicao_id` (`instituicao_id`),
-  CONSTRAINT `doacoes_ibfk_1` FOREIGN KEY (`instituicao_id`) REFERENCES `instituicoes` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
+  `nome` varchar(100) NOT NULL,
+  `descricao` text NOT NULL,
+  `locais` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `instituicoes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -26,8 +25,7 @@ CREATE TABLE `instituicoes` (
   `telefone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `itens` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
